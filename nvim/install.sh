05d67dc8dcd0b9d61925cd662d6c4eda0e9441ab
Felipe Martin <felipemartinrod@gmail.com>
2021-06-16 00:32:34 -0500

    mejora para termux

--- nvim/install.sh
+++ nvim/install.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-sudo apt install neovim -y
+ apt install neovim -y
 
+mkdir ~/.config
 mkdir ~/.config/nvim
 cp init.vim ~/.config/nvim
 
 cp .vimrc ~/.vimrc
 
 mkdir ~/.vim
-mv autoload ~/.vim/autoload
+cp -r autoload ~/.vim/autoload
 
 echo "Ejecutar en neovim:"
 echo "  :PlugInstall"
